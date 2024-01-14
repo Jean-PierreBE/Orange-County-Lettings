fa73ad2c4ee8851c10b3252d04f50ae71e5c586d
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2024-01-14 22:04:44 +0100

    change structures

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -5,13 +5,9 @@ def home(request):
 
 
 def error_404_view(request, exception):
-    # we add the path to the 404.html file
-    # here. The name of our HTML file is 404.html
     return render(request, '404.html')
 
 
 def error_500_view(request):
-    # we add the path to the 404.html file
-    # here. The name of our HTML file is 404.html
     return render(request, '500.html')
 
