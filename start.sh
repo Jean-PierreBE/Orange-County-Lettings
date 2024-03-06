6b0f49569600791f0ba041425f897eb916c1b260
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2024-03-06 20:09:50 +0100

    final touch (i hope)

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ set -e
 
 source /env/bin/activate 
 python manage.py makemigrations 
-python manage.py migrate 
+python manage.py migrate
+python manage.py collectstatic --noinput
 
 exec python manage.py runserver 0.0.0.0:8000
